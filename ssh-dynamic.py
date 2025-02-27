51a79c701da52b995559eb3a9b3f4c8bfda95b7d
isscloudstore <168079123+isscloudstore@users.noreply.github.com>
2025-02-27 10:27:35 +0700

    test change device ip

--- ssh-dynamic.py
+++ ssh-dynamic.py
@@ -1,25 +1,26 @@
 import argparse
 from netmiko import ConnectHandler
 
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Send command to server via SSH")
+parser.add_argument("device_ip", type=str, help="Device Ip address")
+parser.add_argument("dest_ip", type=str, help="Destination Ip address")
+parsed_args = parser.parse_args()
+
 # Server SSH details
 server = {
     "device_type": "linux",
-    "host": "192.168.147.130",  # Change to your server's IP
+    "host": parsed_args.device_ip,  # Change to your server's IP
     "username": "ladmin",
     "password": "P@ssw0rd",
     "port": 22,  # Default SSH port
 }
 
-# Set up argument parser
-parser = argparse.ArgumentParser(description="Send command to server via SSH")
-parser.add_argument("ip", type=str, help="Ip address")
-parsed_args = parser.parse_args()
-
 try:
     # Connect to the server via SSH
     connection = ConnectHandler(**server)
 
-    commands = f"ping -c 5 {parsed_args.ip}"
+    commands = f"ping -c 5 {parsed_args.dest_ip}"
     output = connection.send_command(commands)
     print(output)
 
