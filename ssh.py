ff47b6b2e7331cd2f4ef2b17dc4ee41613e6182c
isscloudstore <168079123+isscloudstore@users.noreply.github.com>
2025-03-01 11:45:10 +0700

    print extract

--- ssh.py
+++ ssh.py
@@ -1,4 +1,5 @@
 from netmiko import ConnectHandler
+import re
 
 # Server SSH details
 server = {
@@ -17,26 +18,33 @@ try:
     connection = ConnectHandler(**server)
 
     # Run a command on the server
-    output = connection.send_command("uname -a")  # Check OS info
-    print(output)
+#    output = connection.send_command("uname -a")  # Check OS info
+#    print(output)
 
     # Run multiple commands
     commands_static = [
-        "uptime",         # Show system uptime
-        "df -h",          # Show disk usage
-        "whoami"          # Show current user
+#       "uptime",         # Show system uptime
+#      "df -h",          # Show disk usage
+#       "whoami"          # Show current user
+        "ifconfig"
     ]
     
-    commands_dynamic = f"ls -l {directory}"
+#    commands_dynamic = f"ls -l {directory}"
 
     for cmd in commands_static:
         response = connection.send_command(cmd)
-        print(f"\nOutput of '{cmd}':\n{response}")
+#        print(f"\nOutput of '{cmd}':\n{response}")
     
-    output = connection.send_command(commands_dynamic)
-    print(f"Output of '{commands_dynamic}':\n{output}")
+#    output = connection.send_command(commands_dynamic)
+#    print(f"Output of '{commands_dynamic}':\n{output}")
     # Close connection
-    connection.disconnect()
+#    connection.disconnect()
+
+        # Regex pattern to match interface name and IP
+        pattern = r"inet\s(\d+)\.(\d+)\.(\d+)\.(\d+)"
+        matches = re.findall(pattern, response)
+
+        print(matches[0][1])
 
 except Exception as e:
     print(f"Error: {e}")
