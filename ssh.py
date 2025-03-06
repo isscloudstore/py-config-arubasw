9fb89c785c5e95f95c9539185c0d81649a0493ca
QTrung ISSVN <Nguyen Quang Trung>
2025-03-06 16:28:35 +0700

    update test ssh aruba

--- ssh.py
+++ ssh.py
@@ -4,36 +4,29 @@ import re
 # Server SSH details
 server = {
     "device_type": "linux",
-    "host": "192.168.147.130",  # Change to your server's IP
-    "username": "ladmin",
-    "password": "P@ssw0rd",
+    "host": "192.168.10.11",  # Change to your server's IP
+    "username": "admin",
+    "password": "",
     "port": 22,  # Default SSH port
 }
-
-# Argument to pass into command
-directory = "/var/log"
 
 try:
     # Connect to the server via SSH
     connection = ConnectHandler(**server)
-
-    # Run a command on the server
-#    output = connection.send_command("uname -a")  # Check OS info
-#    print(output)
 
     # Run multiple commands
     commands_static = [
 #       "uptime",         # Show system uptime
 #      "df -h",          # Show disk usage
 #       "whoami"          # Show current user
-        "ifconfig"
+        "show system"
     ]
     
 #    commands_dynamic = f"ls -l {directory}"
 
     for cmd in commands_static:
         response = connection.send_command(cmd)
-#        print(f"\nOutput of '{cmd}':\n{response}")
+        print(f"\nOutput of '{cmd}':\n{response}")
     
 #    output = connection.send_command(commands_dynamic)
 #    print(f"Output of '{commands_dynamic}':\n{output}")
@@ -41,10 +34,10 @@ try:
 #    connection.disconnect()
 
         # Regex pattern to match interface name and IP
-        pattern = r"inet\s(\d+)\.(\d+)\.(\d+)\.(\d+)"
-        matches = re.findall(pattern, response)
+#        pattern = r"inet\s(\d+)\.(\d+)\.(\d+)\.(\d+)"
+#        matches = re.findall(pattern, response)
 
-        print(matches[0][1])
+#        print(matches[0][1])
 
 except Exception as e:
     print(f"Error: {e}")
