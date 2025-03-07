30d73de617c329d8178723866d53f1e6718a96fa
QTrung ISSVN <Nguyen Quang Trung>
2025-03-07 14:47:37 +0700

    python aruba config version 1.0

--- aruba.py
+++ aruba.py
@@ -1,10 +1,18 @@
 from netmiko import ConnectHandler
 import re
+import argparse
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Send command to server via SSH")
+parser.add_argument("device_ip", type=str, help="Device Ip address")
+parsed_args = parser.parse_args()
+
+
 
 # Aruba switch details
 device = {
     "device_type": "aruba_os",
-    "host": "192.168.10.11",  # Replace with your switch IP
+    "host": parsed_args.device_ip,  # Replace with your switch IP using argparse
     "username": "admin",
     "password": "aruba",
 }
@@ -12,29 +20,29 @@ device = {
 try:
     # Connect to the switch
     connection = ConnectHandler(**device)
-    output=connection.send_command(f"show system\n")
-    pattern = r"Product Name\s+\:\s+(.*?)\s"
-    matches = re.findall(pattern, output)
-    print(matches)
 
-    # Output 1 change username
-    connection.send_command(f"configure terminal\nuser admin group administrators password plaintext aruba\nexit", expect_string=r"#")
-    output1 = connection.send_command(f"show user information", expect_string=r"#")
-    print(output1)
+    # Get Product Name for load configuration
+    output_connection=connection.send_command(f"show system\n")
+    pattern_pn = r"Product Name\s+\:\s+(.*?)\s"
+    matches_pn = re.findall(pattern_pn, output_connection) # Extract Product Name
+    print(matches_pn)
 
-
-    # Change hostname
-    connection.send_command(f"configure terminal\nhostname {matches[0]}\nexit", expect_string=r"#")
-    output2 = connection.send_command(f"show hostname", expect_string=r"#")
-    print(output2)
-
-#   # Exit config mode
-#    connection.send_command("exit", expect_string=r"#")
-
-    # Save configuration
+    # Load configuration from tftp server
+    connection.send_command("copy tftp://192.168.10.1/{matches_pn[0]}.cfg running-config", expect_string=r"#")
     connection.send_command("copy running-config startup-config", expect_string=r"#")
 
-    print("Hostname changed successfully!")
+
+    # Get IP Address for change ip of vlan 1021
+    output_ip_vlan1=connection.send_command(f"show ip int brief\n")
+    pattern_ip_vlan1 = r"(\d+)\.(\d+)\.(\d+)\.(\d+)\/"
+    matches_ip_vlan1 = re.findall(pattern_ip_vlan1, output_ip_vlan1) #Extract IP address of vlan 1
+    print(matches_ip_vlan1)
+
+    # Change IP address off vlan 1021
+    output_change_ip=connection.send_command(f"config\nint vlan 1021\nip addr 10.102.{matches_ip_vlan1[0][2]}.{matches_ip_vlan1[0][3]}/24\nend", expect_string=r"#")
+
+
+    print("Configuration changed successfully!")
     
     # Disconnect from the switch
     connection.disconnect()
